1a57aa5a92671571103435b156a34eec8b1782ee
glaucus03 <glaucus03.tech@gmail.com>
2023-09-12 05:56:32 +0900

    feat: apply volar format

--- frontend/vue-project/src/views/LoginView.vue
+++ frontend/vue-project/src/views/LoginView.vue
@@ -4,21 +4,23 @@
       <div :class="login.left_part">
         <div :class="login.title">Login Page Sample</div>
         <div :class="login.description">
-          Welcome to LoginPageSample! <br>
-            Log in securely and enjoy our platform. <br>
-            New user? Sign up now! Let's get started!<br>
+          Welcome to LoginPageSample! <br />
+          Log in securely and enjoy our platform. <br />
+          New user? Sign up now! Let's get started!<br />
         </div>
         <form id="login-form">
           <label id="login_id_label">User name or Email</label>
-          <input type="text" id="login_id" placeholder="" v-model="inputLoginId">
+          <input type="text" id="login_id" placeholder="" v-model="inputLoginId" />
           <div :class="login.input_underline"></div>
           <label id="login_password_label">Password</label>
-          <input type="password" id="login_password" placeholder="" v-model="inputLoginPassword">
+          <input type="password" id="login_password" placeholder="" v-model="inputLoginPassword" />
           <div :class="login.input_underline"></div>
 
           <div :class="login.forgot_text">Forgot Password?</div>
-          <br>
-          <button type="button" @click="onSignInClick(inputLoginId, inputLoginPassword)">Sign In</button>
+          <br />
+          <button type="button" @click="onSignInClick(inputLoginId, inputLoginPassword)">
+            Sign In
+          </button>
         </form>
       </div>
       <div :class="login.right_part"></div>
@@ -26,41 +28,37 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-import { storeToRefs } from 'pinia';
-import { loginDataStore } from '@/stores/modules/loginData';
+import { ref } from 'vue'
+import { loginDataStore } from '@/stores/modules/loginData'
 
 // setup pinia
-const loginStore = loginDataStore();
-const { reset, onSignInClick } = loginStore;
+const loginStore = loginDataStore()
+const { onSignInClick } = loginStore
 
 // setup filed
-const inputLoginId = ref("");
-const inputLoginPassword = ref("");
-
+const inputLoginId = ref('')
+const inputLoginPassword = ref('')
 </script>
 <style module="login">
-
 .pagebackground {
   position: absolute;
   width: 100vw;
   height: 100vh;
   background-position: top;
-  background-size:cover, contain;
-  background-image: url("@/assets/login-page-background.png");
+  background-size: cover, contain;
+  background-image: url('@/assets/login-page-background.png');
   display: flex;
 }
-
 
 .content_card {
   width: 80vw;
   height: 80vh;
-  margin-top: 5%; 
+  margin-top: 5%;
   margin-left: auto;
   margin-right: auto;
   background-color: white;
-  border-radius:30px;
-  display:flex;
+  border-radius: 30px;
+  display: flex;
 }
 
 .left_part {
@@ -69,22 +67,21 @@ const inputLoginPassword = ref("");
   display: flex;
   flex-direction: column;
   padding-left: 15%;
-  border-radius:30px;
-  text-align:left;
-
+  border-radius: 30px;
+  text-align: left;
 }
 
 .title {
   margin-top: 60px;
   font-size: 2em;
-  font-family: "Times";
+  font-family: 'Times';
   color: black;
 }
 
 .description {
   margin-top: 40px;
   font-size: 1em;
-  font-family: "Times";
+  font-family: 'Times';
   color: grey;
 }
 
@@ -98,7 +95,7 @@ const inputLoginPassword = ref("");
   margin-top: 10px;
   margin-bottom: 50px;
   font-size: 0.75em;
-  font-family: "Times";
+  font-family: 'Times';
   color: grey;
 }
 
@@ -108,36 +105,33 @@ const inputLoginPassword = ref("");
   margin-left: auto;
   margin-right: auto;
   font-size: 80px;
-  color:ghostwhite ;
+  color: ghostwhite;
   display: flex;
   flex-direction: column;
-  border-radius:0 30px 30px 0;
+  border-radius: 0 30px 30px 0;
   background-image: linear-gradient(90deg, rgba(233, 233, 233, 1), rgba(172, 172, 172, 1));
-
 }
 </style>
 
 <style scoped>
-
 #login-form {
   position: relative;
   display: flex;
   flex-direction: column;
-  margin-top:50px;
+  margin-top: 50px;
   width: 50%;
 }
 
 #login_id_label,
-#login_password_label
-{
+#login_password_label {
   margin-top: 30px;
   font-size: 0.75em;
-  font-family: "Times";
+  font-family: 'Times';
   color: grey;
 }
 
 input {
-  border:none;
+  border: none;
   box-shadow: none;
   outline: none;
   margin-left: auto;
@@ -148,8 +142,8 @@ input {
 }
 
 button {
-  border:none;
-  border-radius:30px;
+  border: none;
+  border-radius: 30px;
   box-shadow: none;
   margin-left: auto;
   margin-right: auto;
@@ -159,4 +153,3 @@ button {
   background-image: linear-gradient(90deg, rgba(57, 210, 248, 1), rgba(48, 97, 96, 1));
 }
 </style>
-
